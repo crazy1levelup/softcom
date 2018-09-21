f25a09aa3504465d54057fd2ad8b36ea3d0e52ca
crazy1levelup <trif.eduard@yahoo.ro>
2018-09-21 13:29:53 +0300

    Encapsulation added

---
+++ src/com/firmasoft/eduard/Encap.java
@@ -0,0 +1,28 @@
+package com.firmasoft.eduard;
+
+public class Encap {
+
+    private String name;
+    private String userName;
+    private int number;
+
+    public String getName(){
+        return name;
+    }
+    public String getUserName(){
+        return userName;
+    }
+    public int getNumber(){
+        return number;
+    }
+
+    public void setName(String newName){
+        name = newName;
+    }
+    public void setUserName(String newUserName){
+        userName = newUserName;
+    }
+    public void setNumber(int newNumber){
+        number = newNumber;
+    }
+}
